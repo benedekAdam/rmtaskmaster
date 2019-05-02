083bc8205030f3ce42ef4805527e1651f3f6f433
Benedek Ádám <benedek.adam@introweb.hu>
2019-05-03 01:21:00 +0200

    keys_prod update

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,5 +2,10 @@ module.exports = {
     DB_ADDRESS: process.env.DB_ADDRESS,
     POST_TOKEN: process.env.POST_TOKEN,
     RM_HOST: process.env.RM_HOST,
-    RM_DEFAULT_API_KEY: process.env.RM_DEFAULT_API_KEY
+    RM_HOST_WITHOUT_PROTOCOL: process.env.RM_HOST_WITHOUT_PROTOCOL,
+    RM_DEFAULT_API_KEY: process.env.RM_DEFAULT_API_KEY,
+    PORT: process.env.PORT,
+    HELP_KEYWORD: process.env.HELP_KEYWORD,
+    HELP_CONFIG: process.env.HELP_CONFIG,
+    PRIORITY_COLORS: process.env.PRIORITY_COLORS
 }
